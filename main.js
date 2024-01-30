878104063664c48bd6dbcc2850490a38bef3e560
Caleb-Abuul <abuulcaleb@gmail.com>
2024-01-30 23:38:00 +0100

    good to go

--- main.js
+++ main.js
@@ -1,172 +1,173 @@
 const grabItem = (id) => document.getElementById(id);
-const displayItem = (element, message) => element.innerText = message;
-    
+let ageForm = grabItem('myForm');
+const displayError = (element, message) => document.getElementById(element).innerText = message;
 
-let myForm = grabItem('myForm');
+const displayAge = (element, age) => document.getElementById(element).innerText = age;
+
 let currentDate = new Date();
-let currYear = currentDate.getFullYear();
-let currMonth = currentDate.getMonth() + 1;
-console.log(currMonth);
-let currDay = currentDate.getDate();
+let currentDay = currentDate.getDate();
+let currentMonth = currentDate.getMonth() + 1;
+let currentYear = currentDate.getFullYear();
+let isError = false;
 
 
-let headingDay = document.querySelector('#heading-day');
-let headingMonth = document.querySelector('#heading-month');
-let headingYear = document.querySelector('#heading-year');
-
-let displayYear = grabItem('display-year'), displayMonth = grabItem('display-month'), displayDay = grabItem('display-day');
-
-myForm.addEventListener('submit', (e)=>{
+ageForm.addEventListener('submit', (e)=>{
     e.preventDefault();
 
-    
+    let inputDay = grabItem('bornDay').value.trim();
+    let inputMonth = grabItem('bornMonth').value.trim();
+    let inputYear = grabItem('bornYear').value.trim();
 
-    validateForm();
+    isError = false;
+    refreshState();
+
+    validateMonth(inputMonth);
+    validateDay(inputMonth, inputDay);
+    validateYear(inputYear);
+    if (isError){
+        return;
+    } else {
+        calculateAge(inputYear, inputMonth, inputDay);
+    }
 })
 
-function validateForm(){
-    validateDay();
+function refreshState(){
+    displayError('errorDay', ' ');
+    displayError('errorMonth', ' ');
+    displayError('errorYear', ' ');
+
+    let inputs = ['bornDay', 'bornMonth', 'bornYear'];
+    let headings = ['heading-day', 'heading-month', 'heading-year'];
+
+    inputs.forEach(input =>{
+        grabItem(input).style.border = '2px solid var(--neutral-light-grey)';
+    })
+    headings.forEach(heading =>{
+        grabItem(heading).style.color = 'var(--clr-head)';
+    })
+    
+    displayAge('display-year', '- -');
+    displayAge('display-month', '- -');
+    displayAge('display-day', '- -');
 }
 
-function validateDay(){
-    let inputDay = grabItem('bornDay'), inputMonth = grabItem('bornMonth'), inputYear = grabItem('bornYear');
 
-    let inputMonthValue = inputMonth.value.trim();
-    let inputDayValue = inputDay.value.trim();
-    let inputYearValue = inputYear.value.trim();
+function validateMonth(month){
+    if (month == 0 || month > 12){
+        displayError('errorMonth', 'Must be a valid month');
+        styleErrorinputs();
+    }
+}
 
-    if (inputMonthValue == '04' || inputMonthValue == '06' || inputMonthValue == '09' || inputMonthValue == '11'){
-        if (inputDayValue > 30){
-            let error = document.getElementById('errorDay');
-            displayErrorMessage(error, 'Must be a valid day');
-            return;
-        } 
-    } else if (inputMonthValue > 12){
-        let error = grabItem('errorMonth');
-        displayErrorMessage(error, 'Must be a valid month');
-        return;
-    } else if (inputMonthValue == '02'){
-        if (inputDayValue > 29){
-            let error = grabItem('errorDay');
-            displayErrorMessage(error, 'Must be a valid day');
-            return;
+function validateDay(month, day){
+    if (month == '04' || month == '06' || month == '09' || month == '11'){
+        if (day > 30 || day == 0){
+            displayError('errorDay', 'Must be a valid day');
+            styleErrorinputs();
         }
-    } else if (inputMonthValue == '01' || inputMonthValue == '03' || inputMonthValue == '05' || inputMonthValue == '07' || inputMonthValue == '08' || inputMonthValue == '10' || inputMonthValue == '12'){
-        if (inputDayValue > 31){
-            let error = grabItem('errorDay');
-            displayErrorMessage(error, 'Must be a valid day');
-            return;
+        
+    } else if (month == '01' || month == '03' || month == '05' || month == '07' || month == '08' || month == '10' || month == '12'){
+        if (day > 31 || day == 0){
+            displayError('errorDay', 'Must be a valid day');
+            styleErrorinputs();
+        }
+    } else if (month == '02'){
+        if (day > 29 || day == 0){
+            displayError('errorDay', 'Must be a valid day');
+            styleErrorinputs();
         }
     } else {
-        // let errorDay, errorMonth, errorYear = [grabItem('errorDay'), grabItem('errorMonth'), grabItem('errorYear')];
-
-        let errorDay = grabItem('errorDay');
-        let errorMonth = grabItem('errorMonth');
-        let errorYear = grabItem('errorYear');
-
-        let msg = 'This field is required'
-        displayErrorMessage(errorDay, msg);
-        displayErrorMessage(errorMonth, msg);
-        displayErrorMessage(errorYear, msg);
-
-        return;
-    }
-    if (inputYearValue > currYear){
-        let errorYear = grabItem('errorYear');
-        displayErrorMessage(errorYear, 'Must be in the past');
-        return;
-    }
-
-    // calculate age
-    if (currMonth < inputMonthValue){
-        if (inputDayValue < currDay){
-            let years = (currYear - 1) - inputYearValue;
-            let months = 12 - (inputMonthValue - currMonth);
-            let days = currDay - inputDayValue;
-
-            console.log(years, months, days);
-            console.log('hello');
-
-            displayItem(displayYear, years);
-            displayItem(displayMonth, months);
-            displayItem(displayDay, days);
-        } else if (inputDayValue > currDay){
-            let years = (currYear - 1) - inputYearValue;
-            let months = 11 - (inputMonthValue - currMonth);
-            let days = inputDayValue - currDay;
-
-            console.log(years, months, days);
-            console.log('hello');
-
-            displayItem(displayYear, years);
-            displayItem(displayMonth, months);
-            displayItem(displayDay, days);
-        } else {
-            let years = (currYear - 1) - inputYearValue;
-            let months = 12 - (inputMonthValue - currMonth);
-            let days = currDay - inputDayValue;
-
-            displayItem(displayYear, years);
-            displayItem(displayMonth, months);
-            displayItem(displayDay, days);
-        }
-        
-        
-    } else if (currMonth > inputMonthValue){
-        let years = currYear - inputYearValue;
-        if (inputDayValue < currDay){
-            
-            let months = (inputMonthValue - 1) - currMonth;
-            let days = currDay - inputDayValue;
-
-            displayItem(displayYear, years);
-            displayItem(displayMonth, months);
-            displayItem(displayDay, days);
-        } else if (inputDayValue > currDay){
-            let months = inputMonthValue - currMonth;
-            let days = inputDayValue - currDay;
-
-            displayItem(displayYear, years);
-            displayItem(displayMonth, months);
-            displayItem(displayDay, days);
-        } else {
-            let months = inputMonthValue - currMonth;
-            let days = inputDayValue - currDay;
-
-            displayItem(displayYear, years);
-            displayItem(displayMonth, months);
-            displayItem(displayDay, days);
-        }
-        
-    } else{
-        
-        if (currDay < inputDayValue){
-            let years = (currYear - 1) - inputYearValue;
-            let months = 11 - (currMonth - inputMonthValue);
-            let days = 30 - (inputDayValue - currDay);
-
-            displayItem(displayYear, years);
-            displayItem(displayMonth, months);
-            displayItem(displayDay, days);
-        } else if (currDay > inputDayValue){
-            let years = currYear - inputYearValue;
-            let months = currMonth - inputMonthValue;
-            let days = currDay - inputDayValue;
-
-            displayItem(displayYear, years);
-            displayItem(displayMonth, months);
-            displayItem(displayDay, days);
+        if (day == 0 || day > 31){
+            displayError('errorDay', 'Must be a valid day');
+            styleErrorinputs();
         }
     }
 }
 
-function displayErrorMessage(error, message){
-    error.innerHTML = `<i>${message}</i>`;
-    document.getElementById('bornDay').style.border = '2px solid var(--error-clr)';
-    document.getElementById('bornMonth').style.border = '2px solid var(--error-clr)';
-    document.getElementById('bornYear').style.border = '2px solid var(--error-clr)';
-    headingDay.style.color = 'var(--error-clr)';
-    headingMonth.style.color = 'var(--error-clr)';
-    headingYear.style.color = 'var(--error-clr)';
+function validateYear(year){
+    if (year > currentYear){
+        displayError('errorYear', 'Year must be in the past');
+        styleErrorinputs();
+    }
+    if (year < 1000){
+        displayError('errorYear', 'Year must be in the form YYYY');
+        styleErrorinputs();
+    }
 }
+
+function styleErrorinputs(){
+    let inputs = ['bornDay', 'bornMonth', 'bornYear'];
+    let headings = ['heading-day', 'heading-month', 'heading-year'];
+
+    inputs.forEach(input =>{
+        grabItem(input).style.border = '2px solid var(--error-clr)';
+    })
+    headings.forEach(heading =>{
+        grabItem(heading).style.color = 'var(--error-clr)';
+    })
+    isError = true;
+}
+
+function calculateAge(year, month, day){
+    if (month > currentMonth){
+        let years = (currentYear - 1) - year;
+        if (day > currentDay){
+            let months = 11 - (month - currentMonth);
+            let days = 30 - (day - currentDay);
+
+            showAge(years, months, days);
+        } else if (day < currentDay){
+            let months = 12 - currentMonth;
+            let days = currentDay - day;
+            showAge(years, months, days);
+        } else if (day == currentDay){
+            let months = 12 - currentMonth;
+            let days = currentDay - day;
+            showAge(years, months, days);
+        }
+    } else if (month < currentMonth){
+        let years = currentYear - year;
+        if (day > currentDay){
+            let months = 11 - (month - currentMonth);
+            let days = 30 - (day - currentDay);
+
+            showAge(years, months, days);
+        } else if (day < currentDay){
+            let months = 12 - currentMonth;
+            let days = currentDay - day;
+            showAge(years, months, days);
+        } else if (day == currentDay){
+            let months = 12 - currentMonth;
+            let days = currentDay - day;
+            showAge(years, months, days);
+        }
+    } else if (month == currentMonth){
+        let years = currentYear - year;
+        if (day > currentDay){
+            let years = (currentYear - 1) - year;
+            let months = 11 - (month - currentMonth);
+            let days = 30 - (day - currentDay);
+
+            showAge(years, months, days);
+        } else if (day < currentDay){
+            let months = currentMonth - month;
+            let days = currentDay - day;
+            showAge(years, months, days);
+        } else if (day == currentDay){
+            let months = month - currentMonth;
+            let days = currentDay - day;
+            showAge(years, months, days);
+        }
+    }
+}
+
+function showAge(year, month, day){
+    displayAge('display-year', year);
+    displayAge('display-month', month);
+    displayAge('display-day', day);
+}
+
+
+
+
 
